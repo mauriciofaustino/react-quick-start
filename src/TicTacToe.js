f18ca0b8e2a187f64a4571f98ae65c86da910b25
Mauricio Faustino <mauricio.faustino@gmail.com>
2023-04-15 13:15:12 -0300

    Game working

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -7,12 +7,42 @@ function Square({value, onSquaresClick}) {
 
 export default function TicTacToe() {
     const [squares, setSquares] = useState(Array(9).fill(null))
-    function handleClick(i) {
-        const nextSquares = squares.slice();
-        nextSquares[i] = "X";
-        setSquares(nextSquares)
+    const [xIsNext, setXIsNext] = useState(true)
+
+    function calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
     }
+
+    function handleClick(i) {
+        if (squares[i] || calculateWinner(squares)) return;
+        const nextSquares = squares.slice();
+        nextSquares[i] = xIsNext ? "X" : "O";
+        setSquares(nextSquares);
+        setXIsNext(!xIsNext);
+    }
+
+    const winner = calculateWinner(squares);
+    let status = winner ? "Winner: " + winner : "Next player: " + (xIsNext ? "X":"O")
+
+
     return <>
+        <div className="status">{status}</div>
         <div className="board-row">
             <Square value={squares[0]} onSquaresClick={() => handleClick(0)}/>
             <Square value={squares[1]} onSquaresClick={() => handleClick(1)}/>
