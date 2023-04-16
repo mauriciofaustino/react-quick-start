7493a7c607a8aa5704d0667b2352d3df1707f550
Mauricio Faustino <mauricio.faustino@gmail.com>
2023-04-16 19:42:01 -0300

    Rewrite Board to use two loops to make the squares instead of hardcoding them.

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -35,26 +35,28 @@ function TicTacToe({xIsNext, squares, onPlay}) {
     }
 
     const winner = calculateWinner(squares);
-    let status = winner ? "Winner: " + winner : "Next player: " + (xIsNext ? "X" : "O")
+    const status = winner ? "Winner: " + winner : "Next player: " + (xIsNext ? "X" : "O")
 
+    function renderSquare(i) {
+        return <Square key={i} value={squares[i]} onSquaresClick={() => handleClick(i)}/>
+    }
+
+    function renderBoard() {
+        const boardSize = 3;
+        let boardRows = [];
+        for (let j = 0; j < boardSize; j++) {
+            let boardColumns = [];
+            for (let i = 0; i < boardSize; i++) {
+                boardColumns.push(renderSquare(i * boardSize + j))
+            }
+            boardRows.push(<div key={j} className="board-row">{boardColumns}</div>)
+        }
+        return <>{boardRows}</>
+    }
 
     return <>
         <div className="status">{status}</div>
-        <div className="board-row">
-            <Square value={squares[0]} onSquaresClick={() => handleClick(0)}/>
-            <Square value={squares[1]} onSquaresClick={() => handleClick(1)}/>
-            <Square value={squares[2]} onSquaresClick={() => handleClick(2)}/>
-        </div>
-        <div className="board-row">
-            <Square value={squares[3]} onSquaresClick={() => handleClick(3)}/>
-            <Square value={squares[4]} onSquaresClick={() => handleClick(4)}/>
-            <Square value={squares[5]} onSquaresClick={() => handleClick(5)}/>
-        </div>
-        <div className="board-row">
-            <Square value={squares[6]} onSquaresClick={() => handleClick(6)}/>
-            <Square value={squares[7]} onSquaresClick={() => handleClick(7)}/>
-            <Square value={squares[8]} onSquaresClick={() => handleClick(8)}/>
-        </div>
+        {renderBoard()}
     </>;
 }
 
